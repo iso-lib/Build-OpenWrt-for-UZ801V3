e55695d59d5acfe71966b33f6384464d7a36bc0c
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2024-12-12 02:43:04 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# Add feed source
+
+echo 'src-git-full argon https://github.com/jerrykuku/luci-theme-argon.git' >>feeds.conf.default
+
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
